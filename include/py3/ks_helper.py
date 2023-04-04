37f66bfe59f640261aa416a7d6c0602e57f9c982
Chen Kang Yang <30970698+chen-ky@users.noreply.github.com>
2023-04-04 11:59:11 +1000

    UTF-8 generation now only exclude surrogate codepoint

--- include/py3/ks_helper.py
+++ include/py3/ks_helper.py
@@ -6,13 +6,15 @@ UTF8_CODEPOINT_MIN_RANGE = 0
 # https://en.wikipedia.org/wiki/UTF-8#Encoding
 UTF8_CODEPOINT_MAX_RANGE = 0x10FFFF
 # https://unicodebook.readthedocs.io/unicode_encodings.html#utf-16-surrogate-pairs
-UTF8_CODEPOINT_NO_SURROGATE_MAX_RANGE = 0xD7FF
+UTF8_CODEPOINT_SURROGATE_MIN_RANGE = 0xD800
+UTF8_CODEPOINT_SURROGATE_MAX_RANGE = 0xDFFF
 # https://unicodebook.readthedocs.io/unicode.html#bmp
 UTF8_CODEPOINT_BMP_MAX_RANGE = 0xFFFF
 UTF8_CODEPOINT_ONE_BYTE_MAX_RANGE = 0x007F
 UTF8_CODEPOINT_TWO_BYTE_MAX_RANGE = 0x07FF
 UTF8_CODEPOINT_THREE_BYTE_MAX_RANGE = 0xFFFF
 UTF8_CODEPOINT_FOUR_BYTE_MAX_RANGE = UTF8_CODEPOINT_MAX_RANGE
+ENABLE_UTF8_SURROGATE = False  # Must be False since Python cannot encode surrogate
 
 
 class KsHelper:
@@ -46,9 +48,16 @@ class KsHelper:
             elif bytes_remaining == 2:
                 code_point = self.rng.randint(
                     UTF8_CODEPOINT_MIN_RANGE, UTF8_CODEPOINT_TWO_BYTE_MAX_RANGE)
+            elif bytes_remaining == 3:
+                code_point = self.rng.randint(
+                    UTF8_CODEPOINT_MIN_RANGE, UTF8_CODEPOINT_THREE_BYTE_MAX_RANGE)
             else:
                 code_point = self.rng.randint(
-                    UTF8_CODEPOINT_MIN_RANGE, UTF8_CODEPOINT_NO_SURROGATE_MAX_RANGE)
+                    UTF8_CODEPOINT_MIN_RANGE, UTF8_CODEPOINT_MAX_RANGE)
+
+            if not ENABLE_UTF8_SURROGATE and code_point >= UTF8_CODEPOINT_SURROGATE_MIN_RANGE and code_point <= UTF8_CODEPOINT_SURROGATE_MAX_RANGE:
+                continue
+
             ret += chr(code_point)
             bytes_remaining -= KsHelper._utf8_byte_size(code_point)
         return ret.encode(encoding="utf-8")
