4ebbe2c9c0d37a01e916d473ef9fc9ee6028efed
Chen Kang Yang <30970698+chen-ky@users.noreply.github.com>
2023-08-08 13:03:03 +1000

    Enable unit tests

---
+++ tests/backend/include/py3/test_ks_helper.py
@@ -0,0 +1,22 @@
+from src.backend.include.py3.ks_helper import KsHelper
+
+
+def test_small_rand_bytes():
+    inst = KsHelper()
+    expected_length = 50
+    assert len(inst.rand_bytes(expected_length)) == expected_length
+
+def test_zero_rand_bytes():
+    inst = KsHelper()
+    expected_length = 0
+    assert len(inst.rand_bytes(expected_length)) == expected_length
+
+def test_large_rand_bytes():
+    inst = KsHelper()
+    expected_length = 2**20
+    assert len(inst.rand_bytes(expected_length)) == expected_length
+
+def test_negative_rand_bytes():
+    inst = KsHelper()
+    expected_length = 0
+    assert len(inst.rand_bytes(-1)) == expected_length
