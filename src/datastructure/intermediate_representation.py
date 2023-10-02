0d423994b5295317da57879d951b6013c45eb827
Chen Kang Yang <30970698+chen-ky@users.noreply.github.com>
2023-10-02 11:36:20 +1100

    Add entry point class name to IR

--- src/datastructure/intermediate_representation.py
+++ src/datastructure/intermediate_representation.py
@@ -5,3 +5,4 @@ from typing import Any
 class IntermediateRepresentation():
     def __init__(self, source: dict[str, Any]):
         self.source = source
+        self.entry_point_class_name = source["meta"]["id"]  # TODO job for frontend
