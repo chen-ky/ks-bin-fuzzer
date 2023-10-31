d0797694a1f55d5ed2c644cf207fe0367caef2d8
Chen Kang Yang <30970698+chen-ky@users.noreply.github.com>
2023-10-31 16:47:22 +1100

    Fix enum preprocessor not processing subtypes when there is no enums

--- src/frontend/enum_processor.py
+++ src/frontend/enum_processor.py
@@ -1,5 +1,4 @@
 from typing import Any
-from utils.types import EnumClassEntry
 
 
 class EnumProcessor():
@@ -8,14 +7,14 @@ class EnumProcessor():
 
     def pre_process(self):
         enums = self.source.get("enums")
-        if enums is None:
-            return
-        for enum_class_name, enum_class_items in enums.items():
-            for enum_int_key, enum_val in enum_class_items.items():
-                # Transform enum to verbose enum if needed
-                # https://doc.kaitai.io/user_guide.html#verbose-enums
-                if isinstance(enum_val, str):
-                    enum_class_items[enum_int_key] = {"id": enum_val}
+        if enums is not None:
+            for enum_class_name, enum_class_items in enums.items():
+                for enum_int_key, enum_val in enum_class_items.items():
+                    # Transform enum to verbose enum if needed
+                    # https://doc.kaitai.io/user_guide.html#verbose-enums
+                    if isinstance(enum_val, str):
+                        enum_class_items[enum_int_key] = {"id": enum_val}
+        # Process enums that might be in nested types
         custom_types = self.source.get("types")
         if custom_types is not None:
             for custom_type_src in custom_types.values():
